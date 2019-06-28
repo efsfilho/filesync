39b0237c93f1d802ecbd76b6c4f0f6a0676405cd
efsfilho <efdsfilho@gmail.com>
2019-06-28 18:05:28 -0300

    filehandler.js update

--- fileSyncServer/src/filehandler.js
+++ fileSyncServer/src/filehandler.js
@@ -1,5 +1,6 @@
 const net = require('net');
 const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
 
 class FileHandler {
@@ -30,6 +31,41 @@ class FileHandler {
       stream.on('data', chunk => hash.update(chunk));
       stream.on('end', () => resolve(hash.digest('hex')));
       stream.on('error', err => reject(err));
+    });
+  }
+
+  // Retorna array com nomes e hash de cada arquivo
+  static async montaInfo(files) {
+    let struc = [];
+
+    for (const file of files) {
+      let hash  = await this.criaHash(file).catch(err => err);
+
+      struc.push({
+        filename: path.basename(file),
+        hash: hash,
+        status: 'new'
+      });
+    }
+    return struc;
+  }
+
+  // Verifica existncia de diretorios/arquivos
+  static verificaCaminho(dirPath) {
+    return fs.existsSync(dirPath, err => {
+      console.log(err);
+    });
+  }
+
+  static carregaArquivo(filePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
     });
   }
 }
